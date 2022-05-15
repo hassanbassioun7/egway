52e34b05aa2791aff80717d3b391560cf8d988dc
hassanbassioun7 <97366708+hassanbassioun7@users.noreply.github.com>
2022-05-15 22:05:27 +0200

    Update quality.js

--- JS/quality.js
+++ JS/quality.js
@@ -17,22 +17,10 @@
     });
 }
 
-// Switching between navigations tabs, showing which tab is selected
-// {
-//     const tabs = document.querySelectorAll('[data-tab-target]')
-//     const tabContents = document.querySelectorAll('[data-tab-content]')
+function closeModal(){
+    document.querySelector('.modal').style.display = 'none';
+}
 
-//     tabs.forEach(tab => {
-//         tab.addEventListener('click', () => {
-//             const target = document.querySelector(tab.dataset.tabTarget)
-//             tabContents.forEach(tabContent => {
-//                 tabContent.classList.remove('active')
-//             })
-//             tabs.forEach(tab => {
-//                 tab.classList.remove('active')
-//             })
-//             tab.classList.add('active')
-//             target.classList.add('active')
-//         })
-//     })
-// }
+function openModal(){
+    document.querySelector('.modal').style.display = 'flex';
+}
