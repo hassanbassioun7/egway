664e56dd0e9ab1c2bb5faa8d34d2a144fc5e2d6a
hassanbassioun7 <97366708+hassanbassioun7@users.noreply.github.com>
2022-05-15 22:15:53 +0200

    Update index.php

--- quality/index.php
+++ quality/index.php
@@ -1,5 +1,32 @@
 <?php
 
-header("Location: dashboard.php");
+    include_once("../includes/connection.php");
+    include_once("../includes/functions.php");
 
+    if($user_data = check_login($con)){
+        if($user_data['role'] == 'user'){
+            ?>
+                <script>
+                    alert("You don't have access to this page");
+                    window.location.href='../index.php';
+                </script>
+            <?php
+        }
+        if($user_data['role'] == 'customer service'){
+            ?>
+                <script>
+                    alert("You need to be quality control to access this page");
+                    window.location.href='../service';
+                </script>
+            <?php
+        }
+    }
+    else if(!isset($_SESSION['user_id'])){
+        ?>
+            <script>
+                alert("You don't have access to this page");
+                window.location.href='../index.php';
+            </script>
+        <?php
+    }
 ?>
